2d870caaaad8891c33ee2c1adb6e1512fc4ecd10
Ramazan Sakin <ramazansakin63@gmail.com>
2020-08-10 12:19:50 +0300

    routes defined

---
+++ app/routes/routes.js
@@ -0,0 +1,13 @@
+module.exports = (app) => {
+    const notes = require('../controllers/note-resource.js');
+
+    app.post('/notes', notes.create);
+
+    app.get('/notes', notes.findAll);
+
+    app.get('/notes/:id', notes.findOne);
+
+    app.put('/notes/:id', notes.update);
+
+    app.delete('/notes/:id', notes.delete);
+}
